46f45f621e811e68ccfd192f38a4dc92ba4e7512
ElRafo92 <rafael.martinez@outlook.it>
2022-10-26 10:44:06 +0200

    esercizio mail corretto

--- mail/main.js
+++ mail/main.js
@@ -1,12 +1,19 @@
 // creazione array
 let listaMail = ["rafael@gmial.it", "derian@gmial.it", "cala@gmial.it", "massi@gmial.it", "gaddini@gmial.it", "vale@gmial.it",];
-
 let richiestaMail = prompt("Prego inserire mail:");
+let found = false;
 
 for (let i = 0; i < listaMail.length; i++) {
-    const element = listaMail[i];
-    if (richiestaMail == element) {
-        alert("Welcome");
+    if (richiestaMail === listaMail[i]) {
+        found = true;
         console.log(i)
     }
 }
+
+if (found == true) {
+    alert("Welcome");
+}
+else {
+    alert("You are not registerde");
+}
+
